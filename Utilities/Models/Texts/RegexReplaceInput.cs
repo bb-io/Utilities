dc8a3a3f09f11af6862603ebf309c37691b2dc3b
Cecilia Nistal <cecilia.nistal@centific.com>
2024-04-09 08:25:39 -0300

    Added Regex Replace action

--- Utilities/Models/Texts/RegexReplaceInput.cs
+++ Utilities/Models/Texts/RegexReplaceInput.cs
@@ -2,9 +2,12 @@
 
 namespace Apps.Utilities.Models.Texts
 {
-    public class RegexInput
+    public class RegexReplaceInput
     {
         [Display("Regular Expression")]
         public string Regex { get; set; }
+
+        [Display("Replace Pattern")]
+        public string Replace { get; set; }
     }
 }
