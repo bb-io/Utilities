a21b9f886d50de99fcda80f3acd00ea44a09a947
bZverok <137277669+bZverok@users.noreply.github.com>
2024-04-09 15:53:53 +0300

    added categories

--- Utilities/Application.cs
+++ Utilities/Application.cs
@@ -1,9 +1,16 @@
 ﻿using Blackbird.Applications.Sdk.Common;
+using Blackbird.Applications.Sdk.Common.Metadata;
 
 namespace Apps.Utilities;
 
-public class Application : IApplication
+public class Application : IApplication, ICategoryProvider
 {
+    public IEnumerable<ApplicationCategory> Categories
+    {
+        get => [ApplicationCategory.Utilities];
+        set { }
+    }
+
     public string Name
     {
         get => "Utilities";
