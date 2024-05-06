30b011ca4d7a6fdeda149937ec636bbcca7dfc9b
mathijs-bb <mathijs.sonnemans@blackbird.io>
2024-05-06 16:50:01 +0200

    Added html scrape

--- Utilities/Actions/Scraping.cs
+++ Utilities/Actions/Scraping.cs
@@ -3,13 +3,20 @@ using Blackbird.Applications.Sdk.Common.Invocation;
 using Blackbird.Applications.Sdk.Common;
 using Apps.Utilities.Models.Scraping;
 using HtmlAgilityPack;
+using Apps.Utilities.Models.Files;
+using Blackbird.Applications.SDK.Extensions.FileManagement.Interfaces;
+using System.Text;
 
 namespace Apps.Utilities.Actions
 {
     [ActionList]
     public class Scraping : BaseInvocable
     {
-        public Scraping(InvocationContext context) : base(context) { }
+        private readonly IFileManagementClient _fileManagementClient;
+        public Scraping(InvocationContext context, IFileManagementClient fileManagementClient) : base(context) 
+        {
+            _fileManagementClient = fileManagementClient;
+        }
 
         [Action("Extract web page content", Description = "Get raw and unformatted content from a URL as text")]
         public async Task<ContentDto> ExtractWebContent([ActionParameter][Display("URL")] string url, [ActionParameter][Display("XPath")] string? xpath)
@@ -33,6 +40,34 @@ namespace Apps.Utilities.Actions
                     };
                 }
             }
+        }
+
+        [Action("Extract HTML content", Description = "Get raw and unformatted content from an HTML file")]
+        public async Task<ContentDto> ExtractHtmlContent([ActionParameter] LoadDocumentRequest request, [ActionParameter][Display("XPath")] string? xpath)
+        {
+            var file = await _fileManagementClient.DownloadAsync(request.File);
+
+            var stringBuilder = new StringBuilder();
+            using (var reader = new StreamReader(file))
+            {
+                while (!reader.EndOfStream)
+                {
+                    var line = await reader.ReadLineAsync();
+                    stringBuilder.Append(line);
+                }
+            }
+
+            var content = stringBuilder.ToString();
+
+            var document = new HtmlDocument();
+            document.LoadHtml(content);
+
+            var nodes = document.DocumentNode.SelectNodes(xpath ?? "//*[not(self::style) and not(self::script) and not(self::noscript)]/text()[normalize-space(.) != '']");
+
+            return new ContentDto
+            {
+                Content = nodes == null ? "" : string.Join('\n', nodes.Select(x => HtmlEntity.DeEntitize(x.InnerText.Trim())))
+            };
         }
 
     }
