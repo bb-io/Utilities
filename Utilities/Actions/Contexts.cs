441c68755707be2354593b6d2309c12b235b9624
mathijs-bb <mathijs.sonnemans@blackbird.io>
2024-07-19 14:46:08 +0200

    Added flight context action

---
+++ Utilities/Actions/Contexts.cs
@@ -0,0 +1,29 @@
+﻿using Blackbird.Applications.Sdk.Common.Actions;
+using Blackbird.Applications.Sdk.Common.Invocation;
+using Blackbird.Applications.Sdk.Common;
+using Apps.Utilities.Models.Contexts;
+
+namespace Apps.Utilities.Actions
+{
+    [ActionList]
+    public class Contexts : BaseInvocable
+    {
+        public Contexts(InvocationContext invocationContext) : base(invocationContext)
+        {
+        }
+
+        [Action("Get flight context", Description = "Get various variables that have to do with the flight including IDs, URLs, etc.")]
+        public FlightContext GetFlightContext()
+        {
+            return new()
+            {
+                FlightId = InvocationContext.Flight?.Id.ToString(),
+                FlightUrl = InvocationContext.Flight?.Url,
+                BirdId = InvocationContext.Bird?.Id.ToString(),
+                BirdName = InvocationContext.Bird?.Name,
+                NestId = InvocationContext.Workspace?.Id.ToString(),
+                NestName = InvocationContext.Workspace?.Name
+            };
+        }
+    }
+}
---
+++ Utilities/Models/Contexts/FlightContext.cs
@@ -0,0 +1,30 @@
+﻿using Blackbird.Applications.Sdk.Common;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Apps.Utilities.Models.Contexts
+{
+    public class FlightContext
+    {
+        [Display("Flight ID")]
+        public string? FlightId { get; set; }
+
+        [Display("Flight URL")]
+        public string? FlightUrl { get; set; }
+
+        [Display("Bird ID")]
+        public string? BirdId { get; set; }
+
+        [Display("Bird name")]
+        public string? BirdName { get; set; }
+
+        [Display("Nest ID")]
+        public string? NestId { get; set; }
+
+        [Display("Nest name")]
+        public string? NestName { get; set; }
+    }
+}
