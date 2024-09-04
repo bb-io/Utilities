aa149b35b77bd10eb3925852693b65972add4f35
vitalii-bezuhlyi <vitalii.bezuhlyi@blackbird.io>
2024-09-04 12:32:40 +0300

    Updated docs

--- Utilities/Models/Texts/RegexReplaceInput.cs
+++ Utilities/Models/Texts/RegexReplaceInput.cs
@@ -1,13 +1,12 @@
 ﻿using Blackbird.Applications.Sdk.Common;
 
-namespace Apps.Utilities.Models.Texts
-{
-    public class RegexReplaceInput
-    {
-        [Display("Regular expression")]
-        public string Regex { get; set; }
+namespace Apps.Utilities.Models.Texts;
 
-        [Display("Replace pattern")]
-        public string Replace { get; set; }
-    }
+public class RegexReplaceInput
+{
+    [Display("Regular expression")]
+    public string Regex { get; set; }
+
+    [Display("Replace pattern")]
+    public string Replace { get; set; }
 }
