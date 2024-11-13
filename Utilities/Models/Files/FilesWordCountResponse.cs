13aed62b14e5046c94bea649756a893fb984a99c
vitalii-bezuhlyi <vitalii.bezuhlyi@blackbird.io>
2024-11-13 18:37:50 +0200

    Added Get files word count action

---
+++ Utilities/Models/Files/FilesWordCountResponse.cs
@@ -0,0 +1,21 @@
+﻿using Blackbird.Applications.Sdk.Common;
+
+namespace Apps.Utilities.Models.Files;
+
+public class FilesWordCountResponse
+{
+    [Display("Total word count")]
+    public double WordCount { get; set; }
+
+    [Display("Files with word count")]
+    public List<WordCountItem> FilesWithWordCount { get; set; } = new();
+}
+
+public class WordCountItem
+{
+    [Display("File name")]
+    public string FileName { get; set; } = string.Empty;
+
+    [Display("Word count")]
+    public double WordCount { get; set; }
+}
