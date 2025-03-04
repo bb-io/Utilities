cf95b0d9ad1225a3e28b7812807a2def344fd569
RiabushenkoA <ariabushenko@blackbird.io>
2025-03-04 18:20:23 +0200

    Modified action 'sanitize file name'

--- Tests.Utilities/FileTests.cs
+++ Tests.Utilities/FileTests.cs
@@ -1,5 +1,6 @@
 ﻿using Apps.Utilities.Actions;
 using Apps.Utilities.Models.Files;
+using Apps.Utilities.Models.Shared;
 using Apps.Utilities.Models.Texts;
 using Apps.Utilities.Models.XMLFiles;
 using Blackbird.Applications.Sdk.Common.Exceptions;
@@ -69,6 +70,17 @@ namespace Tests.Utilities
 
             Console.WriteLine(response.Result.Text);
             Assert.IsNotNull(response.Result);
+        }
+
+        [TestMethod]
+        public async Task SanitizeFileName_ReturnsSucces()
+        {
+            var file = new FileDto { File = new FileReference { Name = "test.pdf" } };
+            var request = new SanitizeRequest { FilterCharacters = new List<string> { "t" } };
+            var response = _fileActions.SanitizeFileName(file, request);
+
+            Console.WriteLine(response.File.Name);
+            Assert.IsNotNull(response);
         }
     }
 }
