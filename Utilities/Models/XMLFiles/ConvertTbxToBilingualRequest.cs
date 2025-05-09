e4ed27d67c298028a95e8e55f78dc467a4f8b63d
RiabushenkoA <ariabushenko@blackbird.io>
2025-05-09 12:15:49 +0300

    Added action 'Convert TBX to bilingual'

---
+++ Utilities/Models/XMLFiles/ConvertTbxToBilingualRequest.cs
@@ -0,0 +1,17 @@
+﻿using Blackbird.Applications.Sdk.Common;
+using Blackbird.Applications.Sdk.Common.Files;
+
+namespace Apps.Utilities.Models.XMLFiles
+{
+    public class ConvertTbxToBilingualRequest
+    {
+        [Display("TBX file")]
+        public FileReference File { get; set; }
+
+        [Display("Source language")]
+        public string SourceLanguage { get; set; }
+
+        [Display("Target language")]
+        public string TargetLanguage { get; set; }
+    }
+}
