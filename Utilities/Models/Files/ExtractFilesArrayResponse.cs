d96c0dc38a4d373980d1a098785a47835764cf85
RiabushenkoA <ariabushenko@blackbird.io>
2025-10-06 19:09:29 +0300

    Added actions 'Filter files by name using Regex' and 'Extract matches from array using Regex'

--- Utilities/Models/Files/ExtractFilesArrayResponse.cs
+++ Utilities/Models/Files/ExtractFilesArrayResponse.cs
@@ -1,9 +1,11 @@
-﻿using Blackbird.Applications.Sdk.Common.Files;
+﻿using Blackbird.Applications.Sdk.Common;
+using Blackbird.Applications.Sdk.Common.Files;
 
 namespace Apps.Utilities.Models.Files
 {
     public class ExtractFilesArrayResponse
     {
+        [Display("Fili names")]
         public List<string> Files { get; set; }
     }
 }
