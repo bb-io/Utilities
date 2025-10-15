ea849dcb7f21a276361d30db70cb57a0e6e86617
Anton Fesenko <tohafesenkolovep@gmail.com>
2025-10-15 17:08:00 +0300

    make RowPositionOption.RowPosition nullable, add tests, use file-scoped namespaces

--- Utilities/Models/Files/CsvFile.cs
+++ Utilities/Models/Files/CsvFile.cs
@@ -1,12 +1,8 @@
 ﻿using Blackbird.Applications.Sdk.Common;
 using Blackbird.Applications.Sdk.Common.Files;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Apps.Utilities.Models.Files;
+
 public class CsvFile
 {
     [Display("CSV file")]
