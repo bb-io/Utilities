7ac64f52357c4247cc2954abf82e0bbe91d1df27
Artem Riabushenko <ariabushenko@blackbird.io>
2026-01-05 21:46:11 +0200

    Added new action 'Sum numbers in column'

---
+++ Utilities/Models/Csv/SumNumbersInColumnRequest.cs
@@ -0,0 +1,16 @@
+﻿using Blackbird.Applications.Sdk.Common;
+
+namespace Apps.Utilities.Models.Csv
+{
+    public class SumNumbersInColumnRequest
+    {
+        [Display("Column")]
+        public int ColumnIndex { get; set; }
+
+        [Display("From row", Description = "0-based row index. If empty, starts from the first numeric value in the column.")]
+        public int? FromRow { get; set; }
+
+        [Display("To row", Description = "0-based row index (inclusive). If empty, sums until the end of file.")]
+        public int? ToRow { get; set; }
+    }
+}
---
+++ Utilities/Models/Csv/SumNumbersInColumnResponse.cs
@@ -0,0 +1,18 @@
+﻿using Blackbird.Applications.Sdk.Common;
+
+namespace Apps.Utilities.Models.Csv
+{
+    public class SumNumbersInColumnResponse
+    {
+        public double Sum { get; set; }
+
+        [Display("From row used")]
+        public int FromRowUsed { get; set; }
+
+        [Display("To row used")]
+        public int ToRowUsed { get; set; }
+
+        [Display("Rows processed")]
+        public int RowsProcessed { get; set; }
+    }
+}
