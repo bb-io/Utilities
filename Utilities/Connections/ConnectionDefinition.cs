ddfaa56a97c3e380538b6b809231255a3b8c02a9
Bohdan Makarenko <bohdan.makarenko@blackbird.io>
2026-02-03 11:44:42 +0200

    Revert "Removed connection definiton and connection validator"
    This reverts commit 87e8316208c0044d894cddee57a6b41c41d00623.

---
+++ Utilities/Connections/ConnectionDefinition.cs
@@ -0,0 +1,24 @@
+﻿using Blackbird.Applications.Sdk.Common.Authentication;
+using Blackbird.Applications.Sdk.Common.Connections;
+
+namespace Apps.Utilities.Connections;
+
+public class ConnectionDefinition : IConnectionDefinition
+{
+
+    public IEnumerable<ConnectionPropertyGroup> ConnectionPropertyGroups => new List<ConnectionPropertyGroup>()
+    {
+        new()
+        {
+            Name = "Utilities connection",
+            AuthenticationType = ConnectionAuthenticationType.Undefined,
+            ConnectionProperties = new List<ConnectionProperty>()
+        }
+    };
+
+    public IEnumerable<AuthenticationCredentialsProvider> CreateAuthorizationCredentialsProviders(
+        Dictionary<string, string> values)
+    {
+        return new AuthenticationCredentialsProvider[] { };
+    }
+}
---
+++ Utilities/Connections/ConnectionValidator.cs
@@ -0,0 +1,16 @@
+﻿using Blackbird.Applications.Sdk.Common.Authentication;
+using Blackbird.Applications.Sdk.Common.Connections;
+
+namespace Apps.Utilities.Connections;
+
+public class ConnectionValidator : IConnectionValidator
+{
+    public ValueTask<ConnectionValidationResponse> ValidateConnection(
+        IEnumerable<AuthenticationCredentialsProvider> authProviders, CancellationToken cancellationToken)
+    {
+        return new(Task.FromResult(new ConnectionValidationResponse()
+        {
+            IsValid = true
+        }));
+    }
+}
