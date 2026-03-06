cf4920247597adfdd39cafc33502e5df61fc179b
Alex Terehov <terales@users.noreply.github.com>
2026-03-06 11:25:59 -0330

    Introduce temporary workaround for regex

--- Utilities/Models/Files/ReplaceTextInDocumentRequest.cs
+++ Utilities/Models/Files/ReplaceTextInDocumentRequest.cs
@@ -1,4 +1,6 @@
-﻿using Apps.Utilities.Models.Texts;
+﻿using Apps.Utilities.DataSourceHandlers;
+using Apps.Utilities.Models.Texts;
+using Blackbird.Applications.Sdk.Common.Dictionaries;
 using Blackbird.Applications.Sdk.Common.Files;
 
 namespace Apps.Utilities.Models.Files;
@@ -6,4 +8,7 @@ namespace Apps.Utilities.Models.Files;
 public class ReplaceTextInDocumentRequest : RegexReplaceInput
 {
     public FileReference File { get; set; } = new();
+
+    [StaticDataSource(typeof(CultureSourceHandler))]
+    public string? ExprimentalRegexField { get; set; }
 }
