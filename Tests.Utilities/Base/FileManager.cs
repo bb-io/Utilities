2788e8dd9c16e7f766893c09af30e9dd9a1ff7f2
Fesenko-A <111450737+Fesenko-A@users.noreply.github.com>
2026-04-20 11:55:18 +0300

    fix FileManager does not work on Windows

--- Tests.Utilities/Base/FileManager.cs
+++ Tests.Utilities/Base/FileManager.cs
@@ -1,17 +1,13 @@
 ﻿using Blackbird.Applications.Sdk.Common.Files;
 using Blackbird.Applications.SDK.Extensions.FileManagement.Interfaces;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Tests.Utilities.Base;
+
 public class FileManager(string folderLocation) : IFileManagementClient
 {
     public Task<Stream> DownloadAsync(FileReference reference)
     {
-        var outputPath = Path.Combine(folderLocation, @$"Output/{reference.Name}");
+        var outputPath = Path.Combine(folderLocation, "Output", reference.Name);
         if (File.Exists(outputPath))
         {
             var bytes = File.ReadAllBytes(outputPath);
@@ -19,20 +15,24 @@ public class FileManager(string folderLocation) : IFileManagementClient
             return Task.FromResult((Stream)stream);
         }
 
-        var inputPath = Path.Combine(folderLocation, @$"Input/{reference.Name}");
+        var inputPath = Path.Combine(folderLocation, "Input", reference.Name);
         if (File.Exists(inputPath))
         {
             var bytes = File.ReadAllBytes(inputPath);
             var stream = new MemoryStream(bytes);
             return Task.FromResult((Stream)stream);
         }
-        throw new FileNotFoundException($"File '{reference.Name}' not found in either Input or Output folder.");
+        
+        throw new FileNotFoundException($"File '{reference.Name}' not found. Searched in: {inputPath} and {outputPath}");
     }
 
     public Task<FileReference> UploadAsync(Stream stream, string contentType, string fileName)
     {
-        var path = Path.Combine(folderLocation, @$"Output\{fileName}");
-        new FileInfo(path).Directory.Create();
+        var path = Path.Combine(folderLocation, "Output", fileName);
+        
+        var fileInfo = new FileInfo(path);
+        fileInfo.Directory?.Create();
+
         using (var fileStream = File.Create(path))
         {
             stream.CopyTo(fileStream);
